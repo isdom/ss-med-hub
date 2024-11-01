e32948796f5b8b70ec67b6fa6b5a13cf269af6df
ming.ma <ming.ma@yulore.com>
2024-11-01 18:53:19 +0800

    add L16 playback's command payload

---
+++ src/main/java/com/yulore/asrhub/vo/PayloadPlayback.java
@@ -0,0 +1,12 @@
+package com.yulore.asrhub.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Data
+@ToString
+public class PayloadPlayback {
+    String l16_name;
+}
