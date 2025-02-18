5f1222d208765924d47059350ade42b5020286cf
ming.ma <ming.ma@yulore.com>
2025-02-18 13:16:20 +0800

    add cache=true for wav mode

--- src/main/java/com/yulore/medhub/session/PoActor.java
+++ src/main/java/com/yulore/medhub/session/PoActor.java
@@ -737,7 +737,7 @@ public class PoActor extends ASRActor {
         } else if ("wav".equals(replyVO.getVoiceMode())) {
             return ()->_playbackOn.apply(new PlaybackContext(
                     playbackId,
-                    String.format("{bucket=%s}%s%s", _bucket, _wavPath, replyVO.getAi_speech_file()),
+                    String.format("{cache=true,bucket=%s}%s%s", _bucket, _wavPath, replyVO.getAi_speech_file()),
                     aiContentId));
         } else if ("tts".equals(replyVO.getVoiceMode())) {
             return ()->_playbackOn.apply(new PlaybackContext(
