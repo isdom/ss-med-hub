6999c4c96c5ae3127941cc2a470c257cd0e802ad
ming.ma <ming.ma@yulore.com>
2025-05-12 15:30:40 +0800

    ByteUtil: rename method(_4bytesle_int32/_8bytesle_long)

--- src/main/java/com/yulore/util/ByteUtil.java
+++ src/main/java/com/yulore/util/ByteUtil.java
@@ -1,14 +1,14 @@
 package com.yulore.util;
 
 public class ByteUtil {
-    public static int _4bytes2int32AsLE(final byte[] bytes, final int offset) {
+    public static int _4bytesle_int32(final byte[] bytes, final int offset) {
         return((bytes[offset+3] & 0xFF) << 24) |
             ((bytes[offset+2] & 0xFF) << 16) |
             ((bytes[offset+1] & 0xFF) << 8)  |
             (bytes[offset] & 0xFF);          // 最低有效字节（小端的第一个字节）
     }
 
-    public static long _8bytes2longAsLE(final byte[] bytes, final int offset) {
+    public static long _8bytesle_long(final byte[] bytes, final int offset) {
         return ((bytes[offset+7] & 0xFFL) << 56) |  // 最高有效字节（小端的最后一个字节）
                 ((bytes[offset+5] & 0xFFL) << 40) |
                 ((bytes[offset+6] & 0xFFL) << 48) |
