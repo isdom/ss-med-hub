098d8ef6f62f17b9b3b8c0983856046c9bd22143
ming.ma <ming.ma@yulore.com>
2025-05-18 10:46:27 +0800

    clean import

--- src/main/java/com/yulore/medhub/api/ApplySessionVO.java
+++ src/main/java/com/yulore/medhub/api/ApplySessionVO.java
@@ -1,11 +1,6 @@
 package com.yulore.medhub.api;
 
-import com.google.gson.annotations.SerializedName;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
-
-import java.util.Map;
 
 @Data
 public class ApplySessionVO {
