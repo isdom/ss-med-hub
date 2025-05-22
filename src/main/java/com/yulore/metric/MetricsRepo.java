165a7202a8e23eb6677148d1edb1d1c60b04c9bc
ming.ma <ming.ma@yulore.com>
2025-05-22 18:54:57 +0800

    clean code

--- src/main/java/com/yulore/metric/MetricsRepo.java
+++ src/main/java/com/yulore/metric/MetricsRepo.java
@@ -85,31 +85,6 @@ public class MetricsRepo {
         };
     }
 
-    /*
-    @Bean
-    @Scope(SCOPE_PROTOTYPE)
-    public Gauge buildGauge(final Supplier<Number> f, final String name, final MetricCustomized customized) {
-        log.info("Gauge: create {} with tags:{}", name, customized != null ? customized.tags : "");
-        final var builder = Gauge.builder(name, f)
-                .tags("hostname", HOSTNAME)
-                .tags("ip", LOCAL_IP)
-                .tags("ns", System.getenv("NACOS_NAMESPACE"))
-                .tags("srv", System.getenv("NACOS_DATAID"));
-
-        if (customized != null) {
-            builder.description(customized.description);
-            if (!customized.tags.isEmpty()) {
-                builder.tags(customized.tags.toArray(EMPTY_STRING_ARRAY));
-            }
-            if (!customized.baseUnit.isEmpty()) {
-                builder.baseUnit(customized.baseUnit);
-            }
-        }
-
-        return builder.register(meterRegistry);
-    }
-    */
-
     @Bean
     @Scope(SCOPE_PROTOTYPE)
     public Counter buildCounter(final String name, final MetricCustomized customized) {
