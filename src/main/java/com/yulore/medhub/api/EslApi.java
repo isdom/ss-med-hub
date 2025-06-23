c122918384ad798db306760b1bf03da3ab9c7821
ming.ma <ming.ma@yulore.com>
2025-06-23 16:12:34 +0800

    add EslApi interface

---
+++ src/main/java/com/yulore/medhub/api/EslApi.java
@@ -0,0 +1,58 @@
+package com.yulore.medhub.api;
+
+import feign.Request;
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.TimeUnit;
+
+@FeignClient(
+        value = "${ESL_PROVIDER:unknown-script}",
+        configuration = EslApi.EslApiConfig.class
+)
+public interface EslApi {
+    @Data
+    @ToString
+    class Result {
+        public String   clusterCode;
+        public float    confidence;
+        public String   intentionCode;
+        public String   intentionName;
+    }
+
+    @Data
+    @ToString
+    class Develop {
+        public int  embeddingDuration;
+        public int  searchDuration;
+        public String   vendorName;
+        public String   requestId;
+        public String   code;
+        public String   message;
+    }
+
+    @Data
+    @ToString
+    class SearchResponse {
+        public String   code;
+        public String   message;
+        public Result[] result;
+        public Develop  dev;
+    }
+
+    @RequestMapping(value = "${esl.api.search_text:unknown_ai_api}", method = RequestMethod.GET)
+    SearchResponse search_text(@RequestParam("text") String text);
+
+    // 配置类定义
+    class EslApiConfig {
+        @Bean
+        public Request.Options eslOptions() {
+            // connect(200ms), read(500ms), followRedirects(true)
+            return new Request.Options(200, TimeUnit.MILLISECONDS,  500, TimeUnit.MILLISECONDS,true);
+        }
+    }}
