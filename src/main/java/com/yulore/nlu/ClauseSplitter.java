6db65af0fcb106ebfe76ed8c3485439de115120f
ming.ma <ming.ma@yulore.com>
2025-10-20 18:30:11 +0800

    ClauseSplitter: add splitClausesWithPunctuation & erasePunctuation

--- src/main/java/com/yulore/nlu/ClauseSplitter.java
+++ src/main/java/com/yulore/nlu/ClauseSplitter.java
@@ -9,7 +9,12 @@ public class ClauseSplitter {
             // 中文常见分句标点（全角）
             //"[^，。？！；：]*[，。？！；：]"
             // 不带标点捕获
-            "([^，。？！；：]+)[，。？！；：]"
+            "([^，。？！；：、]+)[，。？！；：、]"
+    );
+
+    static final Pattern cp_pattern_with_punctuation = Pattern.compile(
+            // 中文常见分句标点（全角）
+            "[^，。？！；：、]*[，。？！；：、]"
     );
 
     public static List<String> splitClauses(final String sentence) {
@@ -31,5 +36,28 @@ public class ClauseSplitter {
             }
         }
         return clauses; //.toArray(String[]::new);
+    }
+
+    public static List<String> splitClausesWithPunctuation(final String sentence) {
+
+        final var matcher = cp_pattern_with_punctuation.matcher(sentence);
+        final List<String> clauses = new ArrayList<>();
+        int lastEnd = 0; // 记录上一次匹配结束的位置
+
+        while (matcher.find()) {
+            clauses.add(matcher.group());
+            lastEnd = matcher.end(); // 安全：在 find() 成功后调用
+        }
+
+        // 处理尾部未匹配的部分（例如句子末尾没有标点）
+        if (lastEnd < sentence.length()) {
+            String trailing = sentence.substring(lastEnd);
+            clauses.add(trailing);
+        }
+        return clauses; //.toArray(new String[0]);
+    }
+
+    public static String erasePunctuation(final String clause) {
+        return clause.replaceAll("[，。？！；：、]", "");
     }
 }
