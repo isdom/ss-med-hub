3f8778f51f9acefd8d4b80943fa7eb2e68568bd2
ming.ma <ming.ma@yulore.com>
2025-10-26 20:36:23 +0800

    ClauseSplitter: impl it's Test class, TBD

--- src/main/java/com/yulore/nlu/ClauseSplitterTest.java
+++ src/main/java/com/yulore/nlu/ClauseSplitterTest.java
@@ -2,5 +2,6 @@ package com.yulore.nlu;
 
 public class ClauseSplitterTest {
     public static void main(String[] args) {
+        System.out.printf("%s%n", ClauseSplitter.erasePunctuation("你好啊。"));
     }
 }
