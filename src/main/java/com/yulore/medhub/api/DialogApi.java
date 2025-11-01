f82690c011a11dcd519575a4abea0b44fd414e0d
ming.ma <ming.ma@yulore.com>
2025-11-01 22:15:14 +0800

    add DialogApi for next-dialog-manger srv

---
+++ src/main/java/com/yulore/medhub/api/DialogApi.java
@@ -0,0 +1,31 @@
+package com.yulore.medhub.api;
+
+import feign.Request;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.TimeUnit;
+
+@FeignClient(
+        value = "${dialog.srv}",
+        configuration = DialogApi.ApiConfig.class
+)
+@ConditionalOnProperty(prefix = "dialog", name = "srv")
+public interface DialogApi {
+
+    // 配置类定义
+    class ApiConfig {
+        @Bean
+        public Request.Options options() {
+            // connect(200ms), read(500ms), followRedirects(true)
+            return new Request.Options(200, TimeUnit.MILLISECONDS,  500, TimeUnit.MILLISECONDS,true);
+        }
+    }}
