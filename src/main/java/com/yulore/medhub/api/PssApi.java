550000c764b40524bd8e9932cb0cde6d6572b90f
ming.ma <ming.ma@yulore.com>
2025-12-05 20:51:00 +0800

    PssApi: add log level

--- src/main/java/com/yulore/medhub/api/PssApi.java
+++ src/main/java/com/yulore/medhub/api/PssApi.java
@@ -1,9 +1,11 @@
 package com.yulore.medhub.api;
 
+import feign.Logger;
 import feign.Request;
 import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
@@ -66,5 +68,19 @@ public interface PssApi {
             // connect(200 ms), read(60 minutes), followRedirects(true)
             return new Request.Options(200, TimeUnit.MILLISECONDS,  60, TimeUnit.MINUTES,true);
         }
+
+        @Bean
+        Logger.Level feignLevel() {
+            return switch (_logLevel.toUpperCase()) {
+                case "NONE" -> Logger.Level.NONE;
+                case "BASIC" -> Logger.Level.BASIC;
+                case "HEADERS" -> Logger.Level.HEADERS;
+                case "FULL" -> Logger.Level.FULL;
+                default -> Logger.Level.NONE; // 默认使用NONE级别，避免无效配置导致问题
+            };
+        }
+
+        @Value("${pss.api.log-level:NONE}")
+        private String _logLevel;
     }
 }
