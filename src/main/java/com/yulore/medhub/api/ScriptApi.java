615e1d720df9cc60473692378386523f2266254e
ming.ma <ming.ma@yulore.com>
2025-12-09 11:44:50 +0800

    ScriptApi: add dynamic log level feature

--- src/main/java/com/yulore/medhub/api/ScriptApi.java
+++ src/main/java/com/yulore/medhub/api/ScriptApi.java
@@ -1,9 +1,11 @@
 package com.yulore.medhub.api;
 
+import feign.Logger;
 import feign.Request;
 import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
@@ -136,4 +138,19 @@ public interface ScriptApi {
             // connect(200ms), read(500ms), followRedirects(true)
             return new Request.Options(200, TimeUnit.MILLISECONDS,  500, TimeUnit.MILLISECONDS,true);
         }
-    }}
+
+        @Bean
+        Logger.Level feignLevel() {
+            return switch (_logLevel.toUpperCase()) {
+                case "NONE" -> Logger.Level.NONE;
+                case "BASIC" -> Logger.Level.BASIC;
+                case "HEADERS" -> Logger.Level.HEADERS;
+                case "FULL" -> Logger.Level.FULL;
+                default -> Logger.Level.NONE; // 默认使用NONE级别，避免无效配置导致问题
+            };
+        }
+
+        @Value("${script.api.log-level:NONE}")
+        private String _logLevel;
+    }
+}
