d53f77c42fa83f962bb556e37a91a2f9e1fbadd0
ming.ma <ming.ma@yulore.com>
2026-03-07 17:16:06 +0800

    add field(search_info) for TextGenerationResult

--- src/main/java/com/yulore/aliyun/api/DashScopeApi.java
+++ src/main/java/com/yulore/aliyun/api/DashScopeApi.java
@@ -393,6 +393,8 @@ public interface DashScopeApi {
         private String text;
         private String finish_reason;
         private TextGenerationChoice[]  choices;
+        // 联网搜索到的信息，在设置search_options参数后会返回该参数。
+        private Object search_info;
     }
 
     // REF: https://help.aliyun.com/zh/model-studio/qwen-api-via-dashscope
